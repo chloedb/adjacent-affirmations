e274aae01df4ae21f3b2d9e836bea3dfea1d68b1
Chloe deBeus <chdebeus@davidson.edu>
2020-04-27 15:44:20 -0400

    update the looks and the submit routes

--- routes.js
+++ routes.js
@@ -19,6 +19,10 @@ router.get('/prompts', function(req,res) {
   res.render('prompts')
 })
 
+router.get('/anxietytips', function(req, res) {
+  res.render('panicTips')
+});
+
 router.get('/submit', async function(req, res) {
   let gratitudeMessages = await knex('gratitude_messages').select('*').orderBy('created_at', 'DESC');
 
@@ -32,6 +36,26 @@ router.post('/formSubmission', async function(req, res) {
 
   await knex('gratitude_messages').insert({
     body: body,
+    first_name: first_name,
+    location: location
+  });
+
+  res.redirect('/submit');
+});
+router.post('/formpromptsubmission', async function(req, res) {
+  let body1 = req.body.body1;
+  console.log('this is body1', body1)
+  let body2 = req.body.body2;
+  console.log('this is body2', body2)
+  let body3 = req.body.body3;
+  console.log('this is body3', body3)
+  let body4 = req.body.body4;
+  console.log('this is body4', body4)
+  let first_name = req.body.first_name;
+  let location = req.body.location;
+
+  await knex('gratitude_messages').insert({
+    body: body1 + ', ' + body2 + ', ' + body3 + ', ' + body4,
     first_name: first_name,
     location: location
   });
